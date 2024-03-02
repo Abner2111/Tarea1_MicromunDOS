52e6a09ac55a22837fba06c4f57fd6d0c03bf070
Isriom <isriom@estudiantec.cr>
2024-03-02 11:04:04 -0600

    Dibujado en pantalla finalizado

--- bootloader.asm
+++ bootloader.asm
@@ -1,80 +1,186 @@
-; do addition
-org 0x7c00
-bits 16
-mov ax, 4
-mov bx, 1
-add bx, ax
-mov al, bl; print the number
-mov ah, 0x0e
-add al, 48
-int 0x10
+;codigo original para el debug del boot loader de Michael Stapelberg https://michael.stapelberg.ch/
+;codigo original para el dibujado en pantalla de leeky https://0x00sec.org/t/realmode-assembly-writing-bootable-stuff-part-5/3667
+;Configuracion inicial
+[bits 16]
+mov ah, 0   ;Set display mode
+mov al, 13h ;13h = 320x200, 256 colors
+int  0x10   ;Video BIOS Services
 
 xor ax, ax
+;Pruebas del dibujado
+push 11  ;COLOR
+push 1 ;X-POS
+push 0  ;Y-POS
+call drawBox
+call drawturtle
 
-mov ax, 0xb800
-mov es, ax
-mov byte  ds:[0], 20  ; row
-mov byte  ds:[1], 4   ; col
-mov byte  ds:[2], 39  ; direction
-mov word  ds:[4], 450  ; apple address
-mov word  ds:[6], 80h  ; start address
-mov word  ds:[8], 80h  ; head address
+pop di;Recordar hacer pop, da igual el registro siempre que no este en uso
+pop di
+pop di
+;este conjunto de instrucciones es equivalente a add sp, -3*2
 
-call startupDrawRectangle
+push 0x11; color
+push 1 ;X-POS
+push 3  ;Y-POS
+call drawBox
 
-startupDrawRectangle:
-    push cx
-    push bx
-    
-    ; draw top line
-    mov cx, 80
-    mov bx, 0
-    startupDrawRectangle_topLine:
-        mov byte  es:[bx], 0x00
-        mov byte  es:[bx+1], 255
-        
-        add bx, 2
-        dec cx
-        jnz startupDrawRectangle_topLine
-        
-    ; draw bottom line
-    mov cx, 80
-    mov bx, 3840
-    startupDrawRectangle_bottomLine:
-        mov byte  es:[bx], 0x00
-        mov byte  es:[bx+1], 255
-        
-        add bx, 2
-        dec cx
-        jnz startupDrawRectangle_bottomLine
-        
-            
-    ; draw left line
-    mov cx, 25
-    mov bx, 0
-    startupDrawRectangle_leftLine:
-        mov byte  es:[bx], 0x00
-        mov byte  es:[bx+1], 255
-        
-        add bx, 160
-        dec cx
-        jnz startupDrawRectangle_leftLine
-    
-    ; draw right line
-    mov cx, 25
-    mov bx, 158
-    startupDrawRectangle_rightLine:
-        mov byte  es:[bx], 0x00
-        mov byte  es:[bx+1], 255
-        
-        add bx, 160
-        dec cx
-        jnz startupDrawRectangle_rightLine
-        
-    pop bx
-    pop cx
-    
-    ret 1
+push 0x16; color
+push 1 ;X-POS
+push 2  ;Y-POS
+call drawturtle
+call drawBox
 
+push 0x19; color
+push 1 ;X-POS
+push 1  ;Y-POS
+call drawturtle
+
+jmp end;
+
+drawBox:
+	pusha
+
+	;mover constantes y stackpointer
+    mov si, 6;
+    mov di, 6;
+	mov bx, sp;
+	; recuperar x, y & color
+
+	mov dx, [bx+9*2];
+	mov cx, [bx+9*2+2];
+	mov al, [bx+9*2+4];
+
+    push ax; guardar el color
+    ;Escalar Y
+    mov ax, dx;
+    mul si;
+    mov dx,ax;
+
+    push dx; guardar valor de Y
+
+    ;Escalar X
+    mov ax, cx;
+    mul si; mul sobreescribe dx siempre
+    mov cx,ax;
+	
+	pop dx; recuperar valor de Y
+
+    pop ax
+
+
+	push si               ;save x-length
+	.for_x:
+		push di           ;save y-length
+		.for_y:
+			pusha
+			mov bh, 0     ;page number (0 is default)
+			add cx, si    ;cx = x-coordinate
+			add dx, di    ;dx = y-coordinate
+			mov ah, 0xC   ;write pixel at coordinate
+			int 0x10      ;draw pixel!
+			popa
+		sub di, 1         ;decrease di by one and set flags
+		jnz .for_y        ;repeat for y-length times
+		pop di            ;restore di to y-length
+	sub si, 1             ;decrease si by one and set flags
+	jnz .for_x            ;repeat for x-length times
+	pop si                ;restore si to x-length  -> starting state restored
+	popa
+	ret
+
+drawturtle:
+	pusha
+	;mover constantes y stackpointer
+    mov si, 6;
+    mov di, 6;
+	mov bx, sp;
+	; recuperar x, y del stack
+	mov dx, [bx+9*2];
+	mov cx, [bx+9*2+2];
+
+	;Escalar Y
+    mov ax, dx;
+    mul si;
+    mov dx,ax;
+
+    push dx; guardar valor de Y
+
+    ;Escalar X
+    mov ax, cx;
+    mul si; mul sobreescribe dx siempre
+    mov cx,ax;
+	pop dx;
+	pop ax;
+			;Dibujamos de derecha a izquierda de abajo a arriba
+			;dibujar Linea 6
+			mov bh, 0     ;page number (0 is default)
+			mov al, 40; Color de la tortuga
+
+			add cx, 6    ;cx = x-coordinate
+			add dx, 6    ;dx = y-coordinate
+			mov ah, 0xC   ;write pixel at coordinate
+			int 0x10      ;draw pixel!
+			add cx, -5    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+
+			;Dibujar linea 5
+			add cx, 4    ;cx = x-coordinate
+			add dx, -1    ;dx = y-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+
+			;Dibujar linea 4
+			add cx, 4    ;cx = x-coordinate
+			add dx, -1    ;dx = y-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -3    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+
+			;Dibujar linea 3
+			add cx, 5    ;cx = x-coordinate
+			add dx, -1    ;dx = y-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -3    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+
+			;Dibujar linea 2
+			add cx, 4    ;cx = x-coordinate
+			add dx, -1    ;dx = y-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+
+			;dibujar linea 1
+			add cx, 4    ;cx = x-coordinate
+			add dx, -1    ;dx = y-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -2    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -1    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+			add cx, -2    ;cx = x-coordinate
+			int 0x10      ;draw pixel!
+	popa
+	ret
+
+end:
+	nop
 times 510 - ($-$$) db 0		
 dw 0xAA55
--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 all:
-	nasm PruebaImagen.asm -f elf -o PruebaImagen.o
-	nasm bootloader.asm -f bin  -o bootloader.bin
-	ld -m elf_i386 -s PruebaImagen.o
-	objcopy a.out
-	qemu-system-i386 -fda bootloader.bin
+	nasm bootloader.asm -f elf32 -F dwarf -o bootloader.o
+	ld -Ttext=0x7c00 -m elf_i386 bootloader.o -o bootloader.elf 
+	objcopy -O binary bootloader.elf bootloader.img
+	unset GTK_PATH
+	qemu-system-i386 bootloader.img -s -S &
+	gdb 
